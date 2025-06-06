c6413057e7509e06eff87dfb67a675716775f06f
Her-9704 <oswaldo.199704@gmail.com>
2025-06-06 15:29:59 -0600

    Update App.js

--- EventosComunitarios/App.js
+++ EventosComunitarios/App.js
@@ -1,7 +1,18 @@
 import 'react-native-get-random-values';
 import React from 'react';
 import AppNavigator from './navigation/AppNavigator';
+import { SafeAreaView } from 'react-native-safe-area-context';  
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { UserProvider } from './context/UserContext'; 
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <UserProvider>
+        <SafeAreaView style={{ flex: 1 }}>                         
+        <AppNavigator />
+        </SafeAreaView>
+      </UserProvider>
+    </GestureHandlerRootView>
+  );
 }
